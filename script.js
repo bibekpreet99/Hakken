ce27e6769ddfd1189c205c9160d384b1b4a21866
Bibekpreet Singh <43917624+bibekpreet99@users.noreply.github.com>
2019-02-10 10:15:28 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -135,3 +135,10 @@ document.getElementById("go").addEventListener("click", ()=>{
   console.log(start)
   }
 })
+
+function googleTranslateElementInit() {
+  new google.translate.TranslateElement({pageLanguage: 'en', layout: google.translate.TranslateElement.InlineLayout.SIMPLE}, 'google_translate_element');
+}
+
+
+
